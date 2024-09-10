297a7524e3c3dc15e1ed129186cadf2af37136a1
Mikhail Benkov <m.benkov93@yandex.ru>
2024-09-10 10:33:58 +0300

    made class Node as private static class

--- src/managers/InMemoryHistoryManager.java
+++ src/managers/InMemoryHistoryManager.java
@@ -74,17 +74,19 @@ public class InMemoryHistoryManager implements HistoryManager {
             prevNode.next = nextNode;
         }
     }
-}
 
-class Node<Task> {
+    private static class Node<Task> {
 
-    public Task data;
-    public Node<Task> next;
-    public Node<Task> prev;
+        public Task data;
+        public Node<Task> next;
+        public Node<Task> prev;
 
-    public Node(Node<Task> prev, Task data, Node<Task> next) {
-        this.data = data;
-        this.next = next;
-        this.prev = prev;
+        public Node(Node<Task> prev, Task data, Node<Task> next) {
+            this.data = data;
+            this.next = next;
+            this.prev = prev;
+        }
     }
 }
+
+
